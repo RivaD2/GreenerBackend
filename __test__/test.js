251df1d41dd73631aef5d5d55d3d6b5a9abbcbc4
leahgrace555 <leah.grace555@gmail.com>
2020-12-16 18:46:34 -0500

    continue testing

--- __test__/test.js
+++ __test__/test.js
@@ -1,25 +1,32 @@
 'use strict';
 
-require("@code-fellows/supergoose");
+const supergoose = require("@code-fellows/supergoose");
 const collections = require('../lib/collection/collections.js')
+const { server } = require('../lib/server.js');
+const testServer = supergoose(server);
 
-// describe('testing plants route', ()=> {
-//   it('should add a plant to the database', ()=>{
-//     const plant = {
-//       type: 'cactus',
-//       birthday: new Date(),
-//       feedingFrequency: '2 minutes',
-//       productivityFactor: 1,
-//       maxProductivityFactor: 2,
-//       minProductivityFactor: 0,
-//       cost: 100
-//     };
 
-//     testServer.post('/api/v1/plants').send(plant).then(results => {
-//       expect(results.status).toBe(200);
-//       expect(results.body.type).toEqual('cactus');
-//     })
-//   })
-// })
+describe('testing plants route', ()=> {
+  it('should add a plant to the database', ()=>{
+    const plant = {
+      type: 'cactus',
+      birthday: new Date(),
+      feedingFrequency: '2 minutes',
+      productivityFactor: 1,
+      maxProductivityFactor: 2,
+      minProductivityFactor: 0,
+      cost: 100
+    };
+
+    testServer.post('/api/v1/plants').send(plant).then(results => {
+      console.log(results.status);
+     
+      expect(results.status).toBe(200);
+      expect(results.body.type).toEqual('cactus');
+    })
+  })
+})
+
+
 
 

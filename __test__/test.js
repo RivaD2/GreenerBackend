8f93fbf9fae06ee2c8ab9804576279b2f47ea429
leahgrace555 <leah.grace555@gmail.com>
2020-12-16 16:06:35 -0500

    start testing

---
+++ __test__/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require("@code-fellows/supergoose");
+const collections = require('../lib/collection/collections.js')
+
+// describe('testing plants route', ()=> {
+//   it('should add a plant to the database', ()=>{
+//     const plant = {
+//       type: 'cactus',
+//       birthday: new Date(),
+//       feedingFrequency: '2 minutes',
+//       productivityFactor: 1,
+//       maxProductivityFactor: 2,
+//       minProductivityFactor: 0,
+//       cost: 100
+//     };
+
+//     testServer.post('/api/v1/plants').send(plant).then(results => {
+//       expect(results.status).toBe(200);
+//       expect(results.body.type).toEqual('cactus');
+//     })
+//   })
+// })
+
+

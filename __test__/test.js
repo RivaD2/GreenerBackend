a3d9130696bf3f10f4cab5359e6eca921c140670
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-12-16 17:12:33 -0800

    testing of plant routes and mongo db plant table complete

--- __test__/test.js
+++ __test__/test.js
@@ -4,7 +4,7 @@ const supergoose = require("@code-fellows/supergoose");
 const collections = require('../lib/collection/collections.js')
 const { server } = require('../lib/server.js');
 const testServer = supergoose(server);
-
+let id = '';
 describe('testing plants route', ()=> {
   it('should add a plant to the database', ()=>{
     const plant = {
@@ -17,15 +17,39 @@ describe('testing plants route', ()=> {
       cost: 100
     };
 
-    testServer.post('/api/v1/plants').send(plant).then(results => {
+    return testServer.post('/api/v1/plants').send(plant).then(results => {
      
       expect(results.status).toBe(200);
       expect(results.body.type).toEqual('cactus');
     })
   })
   it('should retrieve plant from the database', ()=>{
-    testServer.get('/api/v1/plants').send().then(results => {
-      console.log(results.body);
+    return testServer.get('/api/v1/plants').send().then(results => {
+      id = results.body[0]._id
+      expect(results.body[0].type).toEqual('cactus');
+      expect(results.status).toEqual(200);
+    })
+  })
+  it('should update the catctus to four leaf clover', () =>{
+    const updated = {
+      type: 'four leaf clover',
+      birthday: new Date(),
+      feedingFrequency: '2 minutes',
+      productivityFactor: 1,
+      maxProductivityFactor: 2,
+      minProductivityFactor: 0,
+      cost: 100
+    };
+    return testServer.put(`/api/v1/plants/${id}`).send(updated).then(res => {
+      console.log(res.body)
+      expect(res.body.type).toEqual('four leaf clover');
+      expect(res.status).toEqual(200);
+    })
+  })
+  it('can delete the plant from the db', () => {
+    return testServer.delete(`/api/v1/plants/${id}`).then(res => {
+      console.log(res.body)
+      expect(res.status).toEqual(200);
     })
   })
 })

e8bb1cf48633b55b0fe79277cdacc78f1b5567eb
leahgrace555 <leah.grace555@gmail.com>
2020-12-16 21:03:07 -0500

    testing terrarium

--- __test__/test.js
+++ __test__/test.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const supergoose = require("@code-fellows/supergoose");
+//const { describe } = require("yargs");
 const collections = require('../lib/collection/collections.js')
 const { server } = require('../lib/server.js');
 const testServer = supergoose(server);
-let id = '';
+let plantId = '';
+let terrariumId = '';
 describe('testing plants route', ()=> {
   it('should add a plant to the database', ()=>{
     const plant = {
@@ -25,7 +27,7 @@ describe('testing plants route', ()=> {
   })
   it('should retrieve plant from the database', ()=>{
     return testServer.get('/api/v1/plants').send().then(results => {
-      id = results.body[0]._id
+      plantId = results.body[0]._id
       expect(results.body[0].type).toEqual('cactus');
       expect(results.status).toEqual(200);
     })
@@ -40,16 +42,32 @@ describe('testing plants route', ()=> {
       minProductivityFactor: 0,
       cost: 100
     };
-    return testServer.put(`/api/v1/plants/${id}`).send(updated).then(res => {
+    return testServer.put(`/api/v1/plants/${plantId}`).send(updated).then(res => {
       console.log(res.body)
       expect(res.body.type).toEqual('four leaf clover');
       expect(res.status).toEqual(200);
     })
   })
   it('can delete the plant from the db', () => {
-    return testServer.delete(`/api/v1/plants/${id}`).then(res => {
+    return testServer.delete(`/api/v1/plants/${plantId}`).then(res => {
       console.log(res.body)
       expect(res.status).toEqual(200);
+    })
+  })
+});
+
+
+describe('testing terrarium database table', ()=> {
+  console.log('i am here');
+  it('should be able to add to the terrarium db', ()=> {
+    const terrarium = {
+      plotSize: 3,
+      cost: 50000,
+    };
+    return testServer.post('/api/v1/terrariums').send(terrarium)
+    .then( result => {
+      console.log(result.body)
+      expect(result.body.plotSize).toEqual(3)
     })
   })
 })
